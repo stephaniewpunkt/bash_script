724ee39186a98e95bc4faa6f1f6086864816fc65
Stephanie Werner <Stephanie.Werner@h-da.de>
2022-03-21 15:08:38 +0100

    initial commit

---
+++ count_lines.sh
@@ -0,0 +1,8 @@
+# Small script to count lines in .tsv files
+# Stephanie Werner <stephanie.werner@h-da.de>
+# 2022-03-21
+
+echo "Counting lines in all tsv files"
+wc -l *tsv
+echo "Done! Have a nice day!"
+
